12dd4e4462281180085b4d0d4fbec2828e1fef61
DrHaartman <noordin.ramadhan1@gmail.com>
2025-12-27 18:00:33 +0300

    second commit for MONGOURI

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
-const port = 3000;
+const port = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+app.post('/register', (req, res) => {
+  const { username, password } = req.body;
+  res.json({requestData: { username, password }});
+  console.log('Register endpoint', username, password);
+});
+
+
 
 app.get('/', (req, res) => {
-  res.send('Tuko on!');
+  res.send('Server is running!');
 });
 
 app.listen(port, () => {
--- Styloque Frontend/src/Components/Pages/LogIn.jsx
+++ Styloque Frontend/src/Components/Pages/LogIn.jsx
@@ -1,11 +1,32 @@
-
+import React, { useState } from 'react';
 
 function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        await fetch('http://localhost:5000/login', {
+            method: 'POST',
+            headers: {'content-type': 'application/json'},
+            body: JSON.stringify({ username, password })
+        })
+    }
+
+
     return (
-        <form className="max-w-md shadow mx-auto my-6 bg-gray-200 p-4 rounded-lg">
+        <form onSubmit={handleSubmit} className="max-w-md shadow mx-auto my-6 bg-gray-200 p-4 rounded-lg">
             <h1 className="text-center text-xl font-bold mb-4">Log In</h1>
-            <input type="text" placeholder="Username" className="w-full p-2 mb-2 border border-gray-300 rounded" />
-            <input type="password" placeholder="Password" className="w-full p-2 mb-2 border border-gray-300 rounded" />
+            <input  type="text" 
+                    placeholder="Enter Username" 
+                    className="w-full p-2 mb-2 border border-gray-300 rounded" 
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}/>
+            <input type="password" 
+                    placeholder="Enter Password" 
+                    className="w-full p-2 mb-2 border border-gray-300 rounded" 
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}/>
             <button type="submit" className="cursor-pointer hover:bg-gray-600 w-full bg-gray-900 text-white p-2 rounded">Log In</button>
         </form>
     );
